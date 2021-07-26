b02d177b479e964b556873d27a851d8b29fb20de
Ta93455 <tta93455@gmail.com>
2021-07-26 04:46:18 -0700

    Simple fizzbuzz

---
+++ FizzBuzz/whileloop.js
@@ -0,0 +1,13 @@
+x = 0
+
+while (x < 100) {
+    x++;
+    if (x % 15 == 0)
+        console.log("FizzBuzz");
+    else if (x % 3 == 0)
+        console.log("Fizz");
+    else if (x % 5 == 0)
+        console.log("Buzz");
+    else
+        console.log(x);
+}
