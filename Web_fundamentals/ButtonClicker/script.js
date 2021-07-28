504e123e0deb041c7f0638f57b2234a2b05e89e8
Ta93455 <tta93455@gmail.com>
2021-07-28 10:04:32 -0700

    Additions

--- Web_fundamentals/ButtonClicker/script.js
+++ Web_fundamentals/ButtonClicker/script.js
@@ -10,6 +10,21 @@ function add(element) {
     element.remove();
 }
 
-function likes(element) {
-    alert("Ninja was Liked!");
+
+var count = 0;
+var countElement = document.querySelector("#LC");
+
+function likes() {
+    count++;
+    countElement.innerText = count + " " + 'Like(s)'
+    alert("Ninja was liked");
+}
+
+
+var count2 = 0;
+var CE2 = document.querySelector("#LC2");
+
+function inc() {
+    count2++;
+    CE2.innerText = count2 + " " + 'Like(s)'
 }
