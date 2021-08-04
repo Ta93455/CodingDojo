a421a539baab140b9fc6bf52f578ed7957ac8404
Ta93455 <tta93455@gmail.com>
2021-08-04 16:10:54 -0700

    challenge

---
+++ PythonStack/fundamentals/fundamentals/funcint1.py
@@ -0,0 +1,55 @@
+#####UPDATE VALUES IN DICTIONARIES AND LISTS
+x = [ [5,2,3], [15,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Bryant'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Andes', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 30} ]
+########################################################################
+####ITERATE THROUGH A LIST OF DICTIONARIES
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+for person in students:
+    for x, v in person.items():
+        print('{}: {}'.format(x, v))
+
+
+
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary2(key, value):
+    for x in value:
+        print(x[key])
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
+
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(dojo):
+    for k, v in dojo.items():
+        print(f"{len(v)} {k.upper()}")
+        for x in v:
+            print(x)
+        print()
+printInfo(dojo)
+
+
+
